4c743e401c614b4580d2029243c547a7db0a57f3
Ruinato <jeansahler@gmail.com>
2014-04-13 09:18:08 +0200

    test1

--- js/index.js
+++ js/index.js
@@ -1,66 +1,30 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function contactsFind_onSuccess(contacts) {
+    alert('Found ' + navigator.contacts.length + ' navigator.contacts.');
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function contactsFind_onError(contactError) { 
+    alert('onError!');
+}
 
-        alert('Received Event: ' + id);
-        
+function onDeviceReady() {
+    var parentElement = document.getElementById("deviceready");
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
 
-        function onSuccess(contacts) {
-            alert('Found ' + navigator.contacts.length + ' navigator.contacts.');
-        };
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
 
-        function onError(contactError) { 
-            alert('onError!');
-        };
+    alert('Received Event: deviceready');
 
-        // find all contacts with 'Bob' in any name field
-        var options      = new ContactFindOptions();
-        options.filter   = "Jean";
-        options.multiple = true;
-        var fields       = ["displayName", "name"];
-        alert('find contacts');
-        navigator.contacts.find(fields, onSuccess, onError, options);
-    };
-};
+    // find all contacts with 'Bob' in any name field
+    var options      = new ContactFindOptions();
+    options.filter   = "Jean";
+    options.multiple = true;
+    var fields       = ["displayName", "name"];
+    alert('find contacts');
+    navigator.contacts.find(fields, contactsFind_onSuccess, contactsFind_onError, options);
+}
+
+function initialize() {
+    document.addEventListener('deviceready', onDeviceReady, false);
+}
