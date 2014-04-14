d5e9de0f7acfac267a495e04cecf56717a677d2c
Ruanito <jeansahler@gmail.com>
2014-04-14 16:42:38 +0200

    ee

--- js/index.js
+++ js/index.js
@@ -1,21 +1,8 @@
 function contactsFind_onSuccess(contacts) {
-    alert('Found ' + contacts.length + ' navigator.contacts.');
-    //var div = document.getElementById("contactList");
-
-    //var p = "";
+    // alert('Found ' + contacts.length + ' navigator.contacts.');
     for( var i = 0; i < contacts.length; i++) {
-         $("#contactList").append('<li class="list-group-item">' + contacts[i].displayName + '</li>');
-        //p = p + "<li class='list-group-item'>" + contacts[i].displayName + "</li>";
+        $("#contactList").append('<li class="list-group-item">' + contacts[i].displayName + '</li>');
     }
-
-    //div.innerHTML = p;
-/*
-    $.each(contacts, function(key, contact) {
-        $("#contactList").append('<li class="list-group-item">' + contact.displayName + '</li>');
-    ));
-
-*/
-
 }
 
 function contactsFind_onError(contactError) { 
@@ -23,24 +10,23 @@ function contactsFind_onError(contactError) {
 }
 
 function getContacts() {
-     var options     = new ContactFindOptions();
+    var options      = new ContactFindOptions();
     options.filter   = "";
     options.multiple = true;
-    var fields       = ["displayName", "name"];
+    var fields       = ["displayName"];
     navigator.contacts.find(fields, contactsFind_onSuccess, contactsFind_onError, options);
-}
-
-function onDeviceReady() {
-    phonegapDeferred.resolve();
 }
 
 function start() {
     getContacts();
 }
 
+function onDeviceReady() {
+    phonegapDeferred.resolve();
+}
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 $( document ).ready(function() {
     jQueryDeferred.resolve();
-    //alert('jQuery Event: document ready');
 });
